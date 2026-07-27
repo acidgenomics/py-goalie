ec880edcd843a381f6e54a736a18e2a9fe7b7132
Michael Steinbaugh <mike@steinbaugh.com>
2026-07-27 14:52:42 -0400

    Apply acidgenomics CSS theme to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ html_theme_options = {
     "github_url": "https://github.com/acidgenomics/py-goalie",
     "logo": {"text": "goalie"},
 }
+html_css_files = ["https://python.acidgenomics.com/css/sphinx.css"]
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
 }
